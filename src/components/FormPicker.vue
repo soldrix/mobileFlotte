e6713fe79fb489af345d993b56754a9a709c9ac6
ethan gouillart <egouillart@litesoft.fr>
2023-02-03 12:00:16 +0100

    debut datepicker

--- src/components/FormPicker.vue
+++ src/components/FormPicker.vue
@@ -1,16 +1,30 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 import { DatePicker } from 'v-calendar';
 import 'v-calendar/dist/style.css';
 
 const date = ref(new Date())
+date.value.setDate(Number(date.value.getDate()) + 35)
+const range = reactive({
+  start: new Date(),
+  end:  date.value
+})
 
 </script>
 <template>
-  <DatePicker v-model="date" />
+  <DatePicker v-model="range" is-range>
+    <template v-slot:="{inputValue, inputEvents}">
+      <div class="group">
+        <label class="button groupItem" for="start">Start</label>
+        <input type="text" id="start" v-model="inputValue.start">
+      </div>
+    </template>
+  </DatePicker>
   <p>Date : {{date.toJSON() }}</p>
 </template>
 
 <style scoped>
-
+@use form {
+  field: label, group,input, button;
+}
 </style>
